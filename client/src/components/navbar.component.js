31ae239170ba30b97331dbbc534f9d65cede568a
Dhananjay-Rohmetra <rohmetradhananjay@gmail.com>
2020-05-03 02:37:32 +0530

    new changes

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -15,7 +15,7 @@ export default class Navbar extends Component{
               <Link to="/create" className="nav-link">Create Exercise Log</Link>
               </li>
               <li className="navbar-item">
-              <Link to="/user" className="nav-link">Create User</Link>
+              <Link to="/user" className="nav-link">Add User</Link>
               </li>
             </ul>
             </div>
